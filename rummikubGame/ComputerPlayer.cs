607a7a925a890e022aef75be20f9ce0ab551f734
Elad Levi <60574244+eladlevi013@users.noreply.github.com>
2022-10-03 18:31:26 +0300

    computer is playing simple

--- rummikubGame/ComputerPlayer.cs
+++ rummikubGame/ComputerPlayer.cs
@@ -19,15 +19,13 @@ namespace rummikubGame
             tiles = new List<Tile>();
             hand = new List<Tile>();
 
-            tiles.Add(new Tile(1, 11));
-            tiles.Add(new Tile(3, 11));
+            //tiles.Add(new Tile(1, 11));
+            //tiles.Add(new Tile(1, 12));
+            //tiles.Add(new Tile(1, 13));
 
-            tiles.Add(new Tile(1, 12));
-            tiles.Add(new Tile(3, 12));
-
-            tiles.Add(new Tile(1, 13));
-            tiles.Add(new Tile(3, 13));
-
+            //tiles.Add(new Tile(3, 11));
+            //tiles.Add(new Tile(3, 12));
+            //tiles.Add(new Tile(3, 13));
 
             // DELETE
             int AFTER_WE_DESTROYED_RANDOMNESS = tiles.Count();
@@ -51,26 +49,26 @@ namespace rummikubGame
             List<Tile> optimal_solution_hand = hand;
             List<List<Tile>> optimal_solution_sequences = extendedSets;
 
-            for(int i=0; i < hand.Count(); i++)
+            List<Tile> temp_tiles = new List<Tile>();
+            for (int j = 0; j < hand.Count(); j++)
+            {
+                if (hand[j] != null)
+                    temp_tiles.Add(hand[j]);
+            }
+            if (extendedSets != null)
+            {
+                for (int j = 0; j < extendedSets.Count(); j++)
+                    for (int k = 0; k < extendedSets[j].Count(); k++)
+                        temp_tiles.Add(extendedSets[j][k]);
+            }
+
+            for (int i=0; i < 14; i++)
             {
                 List<List<Tile>> temp_extendedSets = new List<List<Tile>>();
                 List<Tile> temp_hand = new List<Tile>();
+                List<Tile> temp_tiles_copy = temp_tiles.Select(item => item.Clone(item.getColor(), item.getNumber())).ToList();
 
-                List<Tile> temp_tiles = new List<Tile>();
-                for(int j=0; j < hand.Count(); j++)
-                {
-                    if (hand[i] != null)
-                        temp_tiles.Add(hand[i]);
-                }
-
-                for(int j=0; j < temp_extendedSets.Count(); j++)
-                {
-                    for(int k=0; k < temp_extendedSets[j].Count(); k++)
-                    {
-                        temp_tiles.Add(temp_extendedSets[j][k]);
-                    }
-                }
-                temp_tiles[i] = GameTable.dropped_tiles_stack.Peek();
+                temp_tiles_copy[i] = GameTable.dropped_tiles_stack.Peek();
 
                 // basically first arrange
                 List<List<Tile>> legalSets = new List<List<Tile>>();
@@ -82,14 +80,21 @@ namespace rummikubGame
                 // we would like to stay with the most melds
                 for (int j = 4; j >= 1 && !best_sets_number_found; j--)
                 {
-                    result = meldsSets(temp_tiles, legalSets, 0, 0, j);
+                    result = meldsSets(temp_tiles_copy, legalSets, 0, 0, j);
+                    if (result == null)
+                    {
+                        legalSets = new List<List<Tile>>();
+                        legalSets.Add(new List<Tile>());
+                    }
+                    else
+                        best_sets_number_found = true;
                 }
 
                 // add to hand
-                for (int j = 0; j < temp_tiles.Count(); j++)
+                for (int j = 0; j < temp_tiles_copy.Count(); j++)
                 {
-                    if (temp_tiles[j] != null)
-                        temp_hand.Add(tiles[j]);
+                    if (temp_tiles_copy[j] != null)
+                        temp_hand.Add(temp_tiles[j]);
                 }
 ;
                 // makes sure to pick the best extended sequences
@@ -121,6 +126,8 @@ namespace rummikubGame
             board.setHand(hand);
             board.setSequences(extendedSets);
             GameTable.current_turn = GameTable.HUMAN_PLAYER_TURN;
+            GameTable.ComputerPlayer.board.deleteCards();
+            GameTable.ComputerPlayer.board.generateBoard();
         }
 
         public void firstArrange()
@@ -252,7 +259,6 @@ namespace rummikubGame
                 for (int i = 0; i < tiles.Count(); i++)
                 {
                     Tile t = tiles[i];
-
                     // && !t.isJoker()
                     if (t != null)
                     {
