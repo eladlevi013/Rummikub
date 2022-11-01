3d051012321696433a6972790bfc9d25daef0dfd
Itamarcohen2001 <101175688+Itamarcohen2001@users.noreply.github.com>
2022-11-01 14:21:42 +0200

    dwf
    fr

--- rummikubGame/ComputerBoard.cs
+++ rummikubGame/ComputerBoard.cs
@@ -88,7 +88,7 @@ namespace rummikubGame
             hand.Add(new Tile(3, 5));
             hand.Add(new Tile(3, 6));
             */
-
+            /*
             hand.Add(new Tile(1, 1));
             hand.Add(new Tile(1, 2));
             hand.Add(new Tile(1, 3));
@@ -103,10 +103,10 @@ namespace rummikubGame
             hand.Add(new Tile(1, 11));
             hand.Add(new Tile(3, 11));
             hand.Add(new Tile(0, 11));
-
+            */
 
             // fills the tiles list
-            for (int i = 0; i < GameTable.RUMMIKUB_TILES_IN_GAME-14; i++)
+            for (int i = 0; i < GameTable.RUMMIKUB_TILES_IN_GAME; i++)
                 hand.Add(GameTable.pool.getTile());
         }
 
