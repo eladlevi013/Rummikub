0bbe113213817107d36df9f7cbe298395ce0aa7e
Elad Levi <60574244+eladlevi013@users.noreply.github.com>
2023-03-13 17:08:47 +0200

    init

---
+++ rummikubGame/Views/GameRulesView.Designer.cs
@@ -0,0 +1,120 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+
+namespace Rummikub.Views
+{
+    partial class GameRulesView
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(GameRulesView));
+            this.instructions_groupbox = new System.Windows.Forms.GroupBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.instructions_rtf = new System.Windows.Forms.RichTextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.close_instructions_btn = new System.Windows.Forms.Button();
+            this.instructions_groupbox.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // instructions_groupbox
+            // 
+            this.instructions_groupbox.Controls.Add(this.pictureBox2);
+            this.instructions_groupbox.Controls.Add(this.instructions_rtf);
+            this.instructions_groupbox.Controls.Add(this.pictureBox1);
+            this.instructions_groupbox.Location = new System.Drawing.Point(45, 38);
+            this.instructions_groupbox.Name = "instructions_groupbox";
+            this.instructions_groupbox.Size = new System.Drawing.Size(740, 501);
+            this.instructions_groupbox.TabIndex = 0;
+            this.instructions_groupbox.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox2.BackgroundImage")));
+            this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox2.Location = new System.Drawing.Point(32, 392);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(222, 91);
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // instructions_rtf
+            // 
+            this.instructions_rtf.Location = new System.Drawing.Point(32, 95);
+            this.instructions_rtf.Name = "instructions_rtf";
+            this.instructions_rtf.Size = new System.Drawing.Size(697, 388);
+            this.instructions_rtf.TabIndex = 2;
+            this.instructions_rtf.Text = "";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Location = new System.Drawing.Point(242, 26);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(264, 62);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // close_instructions_btn
+            // 
+            this.close_instructions_btn.BackColor = System.Drawing.Color.Sienna;
+            this.close_instructions_btn.Location = new System.Drawing.Point(268, 555);
+            this.close_instructions_btn.Name = "close_instructions_btn";
+            this.close_instructions_btn.Size = new System.Drawing.Size(309, 49);
+            this.close_instructions_btn.TabIndex = 1;
+            this.close_instructions_btn.Text = "Close Instructions";
+            this.close_instructions_btn.UseVisualStyleBackColor = false;
+            // 
+            // GameRulesView
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(837, 628);
+            this.Controls.Add(this.close_instructions_btn);
+            this.Controls.Add(this.instructions_groupbox);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "GameRulesView";
+            this.Text = "GameRules";
+            this.Load += new System.EventHandler(this.GameRulesView_Load);
+            this.instructions_groupbox.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private GroupBox instructions_groupbox;
+        private Button close_instructions_btn;
+        private PictureBox pictureBox1;
+        private PictureBox pictureBox2;
+        private RichTextBox instructions_rtf;
+    }
+}
---
+++ rummikubGame/Views/RummikubGameView.Designer.cs
@@ -0,0 +1,321 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+
+namespace Rummikub
+{
+    partial class RummikubGameView
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(RummikubGameView));
+            this.sort_value_btn = new System.Windows.Forms.Button();
+            this.computerTiles_groupbox = new System.Windows.Forms.GroupBox();
+            this.sequences_lbl = new System.Windows.Forms.Label();
+            this.partial_sets_lbl = new System.Windows.Forms.Label();
+            this.hand_tiles_lbl = new System.Windows.Forms.Label();
+            this.dropped_tiles_btn = new System.Windows.Forms.Button();
+            this.pool_btn = new System.Windows.Forms.Button();
+            this.dropped_tiles_lbl = new System.Windows.Forms.Label();
+            this.pool_lbl = new System.Windows.Forms.Label();
+            this.current_pool_size_lbl = new System.Windows.Forms.Label();
+            this.game_indicator_lbl = new System.Windows.Forms.Label();
+            this.show_computer_tiles_checkbox = new System.Windows.Forms.CheckBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveGameToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.loadGameToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.gameToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.resetToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.instructionsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.sort_color_btn = new System.Windows.Forms.Button();
+            this.board_panel = new System.Windows.Forms.Panel();
+            this.computerTiles_groupbox.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // sort_value_btn
+            // 
+            this.sort_value_btn.BackColor = System.Drawing.Color.Sienna;
+            this.sort_value_btn.Location = new System.Drawing.Point(502, 674);
+            this.sort_value_btn.Name = "sort_value_btn";
+            this.sort_value_btn.Size = new System.Drawing.Size(148, 32);
+            this.sort_value_btn.TabIndex = 8;
+            this.sort_value_btn.Text = "Sort By Value";
+            this.sort_value_btn.UseVisualStyleBackColor = true;
+            // 
+            // computerTiles_groupbox
+            // 
+            this.computerTiles_groupbox.Controls.Add(this.sequences_lbl);
+            this.computerTiles_groupbox.Controls.Add(this.partial_sets_lbl);
+            this.computerTiles_groupbox.Controls.Add(this.hand_tiles_lbl);
+            this.computerTiles_groupbox.Location = new System.Drawing.Point(18, 39);
+            this.computerTiles_groupbox.Name = "computerTiles_groupbox";
+            this.computerTiles_groupbox.Size = new System.Drawing.Size(576, 301);
+            this.computerTiles_groupbox.TabIndex = 1;
+            this.computerTiles_groupbox.TabStop = false;
+            // 
+            // sequences_lbl
+            // 
+            this.sequences_lbl.AutoSize = true;
+            this.sequences_lbl.Font = new System.Drawing.Font("Segoe UI", 13F);
+            this.sequences_lbl.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.sequences_lbl.Location = new System.Drawing.Point(14, 108);
+            this.sequences_lbl.Name = "sequences_lbl";
+            this.sequences_lbl.Size = new System.Drawing.Size(100, 25);
+            this.sequences_lbl.TabIndex = 11;
+            this.sequences_lbl.Text = "Sequences:";
+            // 
+            // partial_sets_lbl
+            // 
+            this.partial_sets_lbl.AutoSize = true;
+            this.partial_sets_lbl.Font = new System.Drawing.Font("Segoe UI", 13F);
+            this.partial_sets_lbl.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.partial_sets_lbl.Location = new System.Drawing.Point(407, 20);
+            this.partial_sets_lbl.Name = "partial_sets_lbl";
+            this.partial_sets_lbl.Size = new System.Drawing.Size(101, 25);
+            this.partial_sets_lbl.TabIndex = 10;
+            this.partial_sets_lbl.Text = "Partial Sets:";
+            // 
+            // hand_tiles_lbl
+            // 
+            this.hand_tiles_lbl.AutoSize = true;
+            this.hand_tiles_lbl.Font = new System.Drawing.Font("Segoe UI", 13F);
+            this.hand_tiles_lbl.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.hand_tiles_lbl.Location = new System.Drawing.Point(16, 16);
+            this.hand_tiles_lbl.Name = "hand_tiles_lbl";
+            this.hand_tiles_lbl.Size = new System.Drawing.Size(59, 25);
+            this.hand_tiles_lbl.TabIndex = 8;
+            this.hand_tiles_lbl.Text = "Hand:";
+            // 
+            // dropped_tiles_btn
+            // 
+            this.dropped_tiles_btn.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("dropped_tiles_btn.BackgroundImage")));
+            this.dropped_tiles_btn.Location = new System.Drawing.Point(665, 147);
+            this.dropped_tiles_btn.Name = "dropped_tiles_btn";
+            this.dropped_tiles_btn.Size = new System.Drawing.Size(99, 135);
+            this.dropped_tiles_btn.TabIndex = 2;
+            this.dropped_tiles_btn.UseVisualStyleBackColor = true;
+            // 
+            // pool_btn
+            // 
+            this.pool_btn.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pool_btn.BackgroundImage")));
+            this.pool_btn.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pool_btn.Location = new System.Drawing.Point(817, 147);
+            this.pool_btn.Name = "pool_btn";
+            this.pool_btn.Size = new System.Drawing.Size(99, 135);
+            this.pool_btn.TabIndex = 3;
+            this.pool_btn.UseVisualStyleBackColor = true;
+            this.pool_btn.Click += new System.EventHandler(this.pool_btn_Click);
+            // 
+            // dropped_tiles_lbl
+            // 
+            this.dropped_tiles_lbl.AutoSize = true;
+            this.dropped_tiles_lbl.Location = new System.Drawing.Point(680, 131);
+            this.dropped_tiles_lbl.Name = "dropped_tiles_lbl";
+            this.dropped_tiles_lbl.Size = new System.Drawing.Size(73, 13);
+            this.dropped_tiles_lbl.TabIndex = 4;
+            this.dropped_tiles_lbl.Text = "Dropped Tiles";
+            // 
+            // pool_lbl
+            // 
+            this.pool_lbl.AutoSize = true;
+            this.pool_lbl.Location = new System.Drawing.Point(840, 131);
+            this.pool_lbl.Name = "pool_lbl";
+            this.pool_lbl.Size = new System.Drawing.Size(53, 13);
+            this.pool_lbl.TabIndex = 5;
+            this.pool_lbl.Text = "Pool Tiles";
+            // 
+            // current_pool_size_lbl
+            // 
+            this.current_pool_size_lbl.AutoSize = true;
+            this.current_pool_size_lbl.Location = new System.Drawing.Point(845, 285);
+            this.current_pool_size_lbl.Name = "current_pool_size_lbl";
+            this.current_pool_size_lbl.Size = new System.Drawing.Size(51, 13);
+            this.current_pool_size_lbl.TabIndex = 6;
+            this.current_pool_size_lbl.Text = "Pool Size";
+            this.current_pool_size_lbl.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // game_indicator_lbl
+            // 
+            this.game_indicator_lbl.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Bold);
+            this.game_indicator_lbl.ForeColor = System.Drawing.Color.White;
+            this.game_indicator_lbl.Location = new System.Drawing.Point(680, 39);
+            this.game_indicator_lbl.Name = "game_indicator_lbl";
+            this.game_indicator_lbl.Size = new System.Drawing.Size(216, 50);
+            this.game_indicator_lbl.TabIndex = 7;
+            this.game_indicator_lbl.Text = "GAME INDICATOR";
+            this.game_indicator_lbl.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // show_computer_tiles_checkbox
+            // 
+            this.show_computer_tiles_checkbox.AutoSize = true;
+            this.show_computer_tiles_checkbox.Checked = true;
+            this.show_computer_tiles_checkbox.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.show_computer_tiles_checkbox.Location = new System.Drawing.Point(18, 346);
+            this.show_computer_tiles_checkbox.Name = "show_computer_tiles_checkbox";
+            this.show_computer_tiles_checkbox.Size = new System.Drawing.Size(126, 17);
+            this.show_computer_tiles_checkbox.TabIndex = 10;
+            this.show_computer_tiles_checkbox.Text = "Show Computer Tiles";
+            this.show_computer_tiles_checkbox.UseVisualStyleBackColor = true;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.gameToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(5, 2, 0, 2);
+            this.menuStrip1.Size = new System.Drawing.Size(967, 24);
+            this.menuStrip1.TabIndex = 11;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.saveGameToolStripMenuItem,
+            this.loadGameToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // saveGameToolStripMenuItem
+            // 
+            this.saveGameToolStripMenuItem.Name = "saveGameToolStripMenuItem";
+            this.saveGameToolStripMenuItem.Size = new System.Drawing.Size(134, 22);
+            this.saveGameToolStripMenuItem.Text = "Save Game";
+            // 
+            // loadGameToolStripMenuItem
+            // 
+            this.loadGameToolStripMenuItem.Name = "loadGameToolStripMenuItem";
+            this.loadGameToolStripMenuItem.Size = new System.Drawing.Size(134, 22);
+            this.loadGameToolStripMenuItem.Text = "Load Game";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(134, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            // 
+            // gameToolStripMenuItem
+            // 
+            this.gameToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.resetToolStripMenuItem,
+            this.instructionsToolStripMenuItem});
+            this.gameToolStripMenuItem.Name = "gameToolStripMenuItem";
+            this.gameToolStripMenuItem.Size = new System.Drawing.Size(50, 20);
+            this.gameToolStripMenuItem.Text = "Game";
+            // 
+            // resetToolStripMenuItem
+            // 
+            this.resetToolStripMenuItem.Name = "resetToolStripMenuItem";
+            this.resetToolStripMenuItem.Size = new System.Drawing.Size(136, 22);
+            this.resetToolStripMenuItem.Text = "Reset";
+            // 
+            // instructionsToolStripMenuItem
+            // 
+            this.instructionsToolStripMenuItem.Name = "instructionsToolStripMenuItem";
+            this.instructionsToolStripMenuItem.Size = new System.Drawing.Size(136, 22);
+            this.instructionsToolStripMenuItem.Text = "Instructions";
+            // 
+            // sort_color_btn
+            // 
+            this.sort_color_btn.BackColor = System.Drawing.Color.Sienna;
+            this.sort_color_btn.Location = new System.Drawing.Point(335, 674);
+            this.sort_color_btn.Name = "sort_color_btn";
+            this.sort_color_btn.Size = new System.Drawing.Size(148, 32);
+            this.sort_color_btn.TabIndex = 9;
+            this.sort_color_btn.Text = "Sort By Color";
+            this.sort_color_btn.UseVisualStyleBackColor = false;
+            // 
+            // board_panel
+            // 
+            this.board_panel.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("board_panel.BackgroundImage")));
+            this.board_panel.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.board_panel.Location = new System.Drawing.Point(12, 352);
+            this.board_panel.Name = "board_panel";
+            this.board_panel.Size = new System.Drawing.Size(951, 334);
+            this.board_panel.TabIndex = 0;
+            // 
+            // RummikubGameView
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(967, 721);
+            this.Controls.Add(this.sort_color_btn);
+            this.Controls.Add(this.sort_value_btn);
+            this.Controls.Add(this.show_computer_tiles_checkbox);
+            this.Controls.Add(this.game_indicator_lbl);
+            this.Controls.Add(this.current_pool_size_lbl);
+            this.Controls.Add(this.pool_lbl);
+            this.Controls.Add(this.dropped_tiles_lbl);
+            this.Controls.Add(this.pool_btn);
+            this.Controls.Add(this.dropped_tiles_btn);
+            this.Controls.Add(this.computerTiles_groupbox);
+            this.Controls.Add(this.menuStrip1);
+            this.Controls.Add(this.board_panel);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "RummikubGameView";
+            this.Text = "RummikubGameView";
+            this.Load += new System.EventHandler(this.RummikubGameView_Load);
+            this.computerTiles_groupbox.ResumeLayout(false);
+            this.computerTiles_groupbox.PerformLayout();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private GroupBox computerTiles_groupbox;
+        private Button dropped_tiles_btn;
+        private Button pool_btn;
+        private Label dropped_tiles_lbl;
+        private Label pool_lbl;
+        private Label current_pool_size_lbl;
+        private Label game_indicator_lbl;
+        private Label sequences_lbl;
+        private Label partial_sets_lbl;
+        private Label hand_tiles_lbl;
+        private Button sort_value_btn;
+        private CheckBox show_computer_tiles_checkbox;
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem fileToolStripMenuItem;
+        private ToolStripMenuItem saveGameToolStripMenuItem;
+        private ToolStripMenuItem loadGameToolStripMenuItem;
+        private ToolStripMenuItem exitToolStripMenuItem;
+        private ToolStripMenuItem gameToolStripMenuItem;
+        private ToolStripMenuItem resetToolStripMenuItem;
+        private ToolStripMenuItem instructionsToolStripMenuItem;
+        private Button sort_color_btn;
+        private Panel board_panel;
+    }
+}
